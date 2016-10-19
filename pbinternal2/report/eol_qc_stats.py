62b9be5d8f24cd5c3eda03e693b53a6e1339aa37
Jayson Falkner <jfalkner@pacificbiosciences.com>
2016-10-19 15:44:02 -0400

    Simplify ternary op use. ITG-178

--- pbinternal2/report/eol_qc_stats.py
+++ pbinternal2/report/eol_qc_stats.py
@@ -252,8 +252,8 @@ def eol_qc_movie_stats(sset, aset, outcsv, nproc=1):
     # pull the "frozen" host and version from ENV
     if 'SMRTLINK_HOST' not in os.environ or 'SMRTLINK_VERSION' not in os.environ:
         log.warn("Must set SMRTLINK_HOST and SMRTLINK_VERSION env variables to note 'frozen' version of the code")
-    smrtlink_host = os.environ['SMRTLINK_HOST'] if 'SMRTLINK_HOST' in os.environ else 'smrtlink-host'
-    smrtlink_version = os.environ['SMRTLINK_VERSION'] if 'SMRTLINK_VERSION' in os.environ else  ''
+    smrtlink_host = os.environ.get('SMRTLINK_HOST', 'smrtlink-host')
+    smrtlink_version = os.environ.get('SMRTLINK_VERSION', '')
     csv = []
     start = time.clock()
     # Rearranged as per Remy's list in ITG-85: https://jira.pacificbiosciences.com/browse/ITG-85
